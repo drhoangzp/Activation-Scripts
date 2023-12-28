ce9e0c1f18c542c6af2595c5a1a0b4cb18c78ad8
drhoangzp <114561932+drhoangzp@users.noreply.github.com>
2023-12-28 12:31:59 +0700

    Update get.ps1

--- get.ps1
+++ get.ps1
@@ -1,29 +1,26 @@
-# Check the instructions here on how to use it https://massgrave.dev/
-
 $ErrorActionPreference = "Stop"
-# Enable TLSv1.2 for compatibility with older clients
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 
-$DownloadURL = 'https://raw.githubusercontent.com/drhoangzp/Activation-Scripts/master/MAS/All-In-One-Version/MAS_AIO.cmd'
-$DownloadURL2 = 'https://bitbucket.org/WindowsAddict/microsoft-activation-scripts/raw/master/MAS/All-In-One-Version/MAS_AIO.cmd'
+$DownloadURLs = @('https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/master/MAS/All-In-One-Version/MAS_AIO.cmd', 'https://bitbucket.org/WindowsAddict/microsoft-activation-scripts/raw/master/MAS/All-In-One-Version/MAS_AIO.cmd')
 
 $rand = Get-Random -Maximum 99999999
-$isAdmin = [bool]([Security.Principal.WindowsIdentity]::GetCurrent().Groups -match 'S-1-5-32-544')
+$isAdmin = bool.Groups -match 'S-1-5-32-544')
 $FilePath = if ($isAdmin) { "$env:SystemRoot\Temp\MAS_$rand.cmd" } else { "$env:TEMP\MAS_$rand.cmd" }
 
-try {
-    $response = Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing
-}
-catch {
-    $response = Invoke-WebRequest -Uri $DownloadURL2 -UseBasicParsing
+foreach ($DownloadURL in $DownloadURLs) {
+    try {
+        $response = Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing
+        break
+    }
+    catch {
+        continue
+    }
 }
 
-$ScriptArgs = "$args "
 $prefix = "@REM $rand `r`n"
 $content = $prefix + $response
 Set-Content -Path $FilePath -Value $content
 
-Start-Process $FilePath $ScriptArgs -Wait
+Start-Process $FilePath "$args " -Wait
 
-$FilePaths = @("$env:TEMP\MAS*.cmd", "$env:SystemRoot\Temp\MAS*.cmd")
-foreach ($FilePath in $FilePaths) { Get-Item $FilePath | Remove-Item }
+Get-ChildItem -Path "$env:TEMP\MAS*.cmd", "$env:SystemRoot\Temp\MAS*.cmd" | Remove-Item
